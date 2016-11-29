81aa2d31ce110dc3d5ac606714c84abb8c425a5a
Степанцев Альберт <126@gosbroker.ru>
2016-11-29 12:06:23 +0300

    + Statement class and tests

--- tests/Dbal/StatementTest.php
+++ tests/Dbal/StatementTest.php
@@ -4,6 +4,20 @@ namespace Running\tests\Dbal\Statement;
 
 use Running\Dbal\Statement;
 
+class testStatement extends Statement
+{
+    public function fetchColumn($column_number = 0)
+    {
+        echo 'arg:' . $column_number;
+    }
+
+    public function fetchAll($fetch_style = null, $fetch_argument = null, $ctor_args = null)
+    {
+        echo 'style:' . $fetch_style;
+        echo 'argument:' . $fetch_argument;
+    }
+}
+
 class StatementTest extends \PHPUnit_Framework_TestCase
 {
 
@@ -12,6 +26,20 @@ class StatementTest extends \PHPUnit_Framework_TestCase
         $sth = new Statement();
         $this->assertInstanceOf(\PDOStatement::class, $sth);
         $this->assertInstanceOf(Statement::class, $sth);
+    }
+
+    public function testFetchScalar()
+    {
+        $sth = new testStatement();
+        $this->expectOutputString('arg:0');
+        $sth->fetchScalar();
+    }
+
+    public function testFetchAllObjects()
+    {
+        $sth = new testStatement();
+        $this->expectOutputString('style:' . \PDO::FETCH_CLASS . 'argument:Test');
+        $sth->fetchAllObjects('Test');
     }
 
 }
