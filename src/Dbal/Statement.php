344ececce15abb7592b0fc3bc17dfece60bb2bf8
Albert Stepantsev <sas@weare.lt>
2017-08-01 15:30:00 +0300

    + Statement class development

--- src/Dbal/Statement.php
+++ src/Dbal/Statement.php
@@ -13,6 +13,7 @@ class Statement
 {
 
     /**
+     * Returns one (first) scalar column from the result
      * @return string
      */
     public function fetchScalar()
@@ -21,12 +22,14 @@ class Statement
     }
 
     /**
-     * @param string $class
+     * Returns an array containing all of the result set rows as instances of the specified class, mapping the columns of each row to named properties in the class
+     * @param string $class Class name
+     * @param array $args Arguments of custom class constructor
      * @return array
      */
-    public function fetchAllObjects($class)
+    public function fetchAllObjects($class, ...$args)
     {
-        return $this->fetchAll(\PDO::FETCH_CLASS, $class);
+        return $this->fetchAll(\PDO::FETCH_CLASS, $class, $args);
     }
 
 }
--- tests/Dbal/StatementTest.php
+++ tests/Dbal/StatementTest.php
@@ -11,10 +11,11 @@ class testStatement extends Statement
         echo 'arg:' . $column_number;
     }
 
-    public function fetchAll($fetch_style = null, $fetch_argument = null, $ctor_args = null)
+    public function fetchAll($fetch_style = null, $fetch_argument = null, $ctor_args = [])
     {
         echo 'style:' . $fetch_style;
         echo 'argument:' . $fetch_argument;
+        echo 'ctor:' . json_encode($ctor_args);
     }
 }
 
@@ -38,8 +39,15 @@ class StatementTest extends \PHPUnit_Framework_TestCase
     public function testFetchAllObjects()
     {
         $sth = new testStatement();
-        $this->expectOutputString('style:' . \PDO::FETCH_CLASS . 'argument:Test');
+        $this->expectOutputString('style:' . \PDO::FETCH_CLASS . 'argument:Test' . 'ctor:[]');
         $sth->fetchAllObjects('Test');
+    }
+
+    public function testFetchAllObjectsWithArgs()
+    {
+        $sth = new testStatement();
+        $this->expectOutputString('style:' . \PDO::FETCH_CLASS . 'argument:Test' . 'ctor:[1,2,3]');
+        $sth->fetchAllObjects('Test', 1, 2, 3);
     }
 
 }
